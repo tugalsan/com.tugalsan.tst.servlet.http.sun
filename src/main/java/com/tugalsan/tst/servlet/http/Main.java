78fa7059da72c3d226bc537ee4b14c6c947fd77c
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-10 16:23:11 +0300

    .

---
+++ src/main/java/com/tugalsan/tst/servlet/http/Main.java
@@ -0,0 +1,24 @@
+package com.tugalsan.tst.servlet.http;
+
+import com.sun.net.httpserver.SimpleFileServer;
+import com.tugalsan.api.file.client.TGS_FileTypes;
+import com.tugalsan.api.servlet.http.server.TS_SHttpHandlerText;
+import com.tugalsan.api.servlet.http.server.TS_SHttpServer;
+import com.tugalsan.api.tuple.client.TGS_Tuple2;
+import java.nio.file.Path;
+
+public class Main {
+
+    //HOW TO EXECUTE
+    //C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.servlet.http>target\bin\webapp.bat
+    public static void main(String[] args) {
+        //TS_SHttpsServerSimple.main(args);
+        TS_SHttpServer.startHttpsServlet("localhost", 8081,
+                Path.of("D:\\xampp_data\\SSL\\tomcat.p12"), "MyPass",
+                Path.of("D:\\xampp_data\\SSL\\"),
+                TS_SHttpHandlerText.of("/", he -> {
+                    return TGS_Tuple2.of(TGS_FileTypes.txt_utf8, "ali gel ğüĞÜöçÖÇşiŞİıI");
+                })
+        );
+    }
+}
