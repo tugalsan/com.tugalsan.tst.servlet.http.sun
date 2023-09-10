6c5dbc1b859b6eb9734c136762ac5c0c91f1e75a
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-10 21:42:59 +0300

    Update Main.java

--- src/main/java/com/tugalsan/tst/servlet/http/Main.java
+++ src/main/java/com/tugalsan/tst/servlet/http/Main.java
@@ -1,25 +1,31 @@
 package com.tugalsan.tst.servlet.http;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.tugalsan.api.callable.client.TGS_CallableType1;
 import com.tugalsan.api.file.client.TGS_FileTypes;
+import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.servlet.http.server.TS_SHttpConfigNetwork;
 import com.tugalsan.api.servlet.http.server.TS_SHttpConfigSSL;
 import com.tugalsan.api.servlet.http.server.TS_SHttpHandlerText;
 import com.tugalsan.api.servlet.http.server.TS_SHttpServer;
 import com.tugalsan.api.tuple.client.TGS_Tuple2;
+import com.tugalsan.api.url.client.parser.TGS_UrlParser;
+import com.tugalsan.api.validator.client.TGS_ValidatorType1;
 import java.nio.file.Path;
 
 public class Main {
 
+    final private static TS_Log d = TS_Log.of(true, Main.class);
+
     //HOW TO EXECUTE
     public static void main(String[] args) {
-        TGS_CallableType1<TGS_Tuple2<TGS_FileTypes, String>, HttpExchange> customHandler = he -> {
-            return TGS_Tuple2.of(TGS_FileTypes.txt_utf8, "ali gel ğüĞÜöçÖÇşiŞİıI");
+        TGS_ValidatorType1<TGS_UrlParser> allow = parser -> {
+            return true;
         };
+        var customTextHandler = TS_SHttpHandlerText.of("/", parser -> {
+            return TGS_Tuple2.of(TGS_FileTypes.txt_utf8, "ali gel ğüĞÜöçÖÇşiŞİıI");
+        });
         var network = TS_SHttpConfigNetwork.of("localhost", 8081);
         var ssl = TS_SHttpConfigSSL.of(Path.of("D:\\xampp_data\\SSL\\tomcat.p12"), "MyPass");
         var folder = Path.of("D:\\file");
-        TS_SHttpServer.startHttpsServlet(network, ssl, folder, TS_SHttpHandlerText.of("/", customHandler));
+        TS_SHttpServer.startHttpsServlet(network, ssl, allow, folder, customTextHandler);
     }
 }
