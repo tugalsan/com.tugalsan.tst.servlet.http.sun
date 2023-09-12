72ded0b7e63dfc570d9854e5860e9aeea142d00f
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-12 16:08:43 +0300

    Update Main.java

--- src/main/java/com/tugalsan/tst/servlet/http/Main.java
+++ src/main/java/com/tugalsan/tst/servlet/http/Main.java
@@ -24,7 +24,7 @@ public class Main {
             }
             return true;
         };
-        var customHandler = TS_SHttpHandlerString.of("/", allow, request -> {
+        var stringHandler = TS_SHttpHandlerString.of("/", allow, request -> {
             return TGS_Tuple2.of(TGS_FileTypes.htm_utf8, TGS_StringUtils.concat(
                     "<html><head><script>location.reload();</script></head><body>",
                     request.url.toString(),
@@ -35,7 +35,7 @@ public class Main {
                 TS_SHttpConfigNetwork.of(settings.ip, settings.sslPort),
                 TS_SHttpConfigSSL.of(settings.sslPath, settings.sslPass, settings.redirectToSSL),
                 TS_SHttpConfigHandlerFile.of(settings.fileHandlerServletName, allow, settings.fileHandlerRoot),
-                customHandler
+                stringHandler
         );
     }
 }
