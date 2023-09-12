669a1f1c28b59d5e94d719cc9ba0204aee9fcf5a
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-12 09:28:42 +0300

    .

--- src/main/java/com/tugalsan/tst/servlet/http/Main.java
+++ src/main/java/com/tugalsan/tst/servlet/http/Main.java
@@ -11,7 +11,6 @@ import com.tugalsan.api.tuple.client.TGS_Tuple2;
 import com.tugalsan.api.url.client.TGS_Url;
 import com.tugalsan.api.url.client.parser.TGS_UrlParser;
 import com.tugalsan.api.validator.client.TGS_ValidatorType1;
-import java.nio.file.Path;
 
 public class Main {
 
@@ -22,6 +21,7 @@ public class Main {
     //cd C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.serialcom
     //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.serialcom-1.0-SNAPSHOT-jar-with-dependencies.jar    
     public static void main(String[] args) {
+        var settings = Settings.of(Settings.pathDefault());
         TGS_ValidatorType1<TGS_UrlParser> allow = parser -> true;
         var customTextHandler = TS_SHttpHandlerText.of("/", allow, httpExchange -> {
             var uri = TS_SHttpUtils.getURI(httpExchange).orElse(null);
@@ -33,9 +33,8 @@ public class Main {
             var parser = TGS_UrlParser.of(TGS_Url.of(uri.toString()));
             return TGS_Tuple2.of(TGS_FileTypes.htm_utf8, uri.toString() + "<br>" + parser.toString());
         });
-        var network = TS_SHttpConfigNetwork.of("localhost", 8081);
-        var ssl = TS_SHttpConfigSSL.of(Path.of("D:\\xampp_data\\SSL\\tomcat.p12"), "MyPass", true);
-        var folder = Path.of("D:\\file");
-        TS_SHttpServer.startHttpsServlet(network, ssl, allow, folder, customTextHandler);
+        var network = TS_SHttpConfigNetwork.of(settings.ip, settings.sslPort);
+        var ssl = TS_SHttpConfigSSL.of(settings.sslPath, settings.sslPass, settings.redirectToSSL);
+        TS_SHttpServer.startHttpsServlet(network, ssl, allow, settings.pathFileServer, customTextHandler);
     }
 }
---
+++ src/main/java/com/tugalsan/tst/servlet/http/Settings.java
@@ -0,0 +1,51 @@
+package com.tugalsan.tst.servlet.http;
+
+import com.tugalsan.api.file.properties.server.TS_FilePropertiesUtils;
+import com.tugalsan.api.file.server.TS_FileUtils;
+import com.tugalsan.api.file.server.TS_PathUtils;
+import com.tugalsan.api.log.server.TS_Log;
+import com.tugalsan.api.unsafe.client.TGS_UnSafe;
+import java.nio.file.Path;
+import java.util.Properties;
+
+public class Settings {
+
+    final private static TS_Log d = TS_Log.of(true, Settings.class);
+
+    public static Path pathDefault() {
+        return TS_PathUtils.getPathCurrent_nio(Settings.class.getName() + ".properties");
+    }
+
+    private Settings(Path propsFile) {
+        var propsExists = TS_FileUtils.isExistFile(propsFile);
+        var props = propsExists ? TS_FilePropertiesUtils.read(propsFile) : new Properties();
+        ip = TS_FilePropertiesUtils.getValue(props, "com.tugalsan.gvm.http.Main_ip", "localhost");
+        d.ci("construtor", "ip", ip);
+        var redirectToSSLStr = TS_FilePropertiesUtils.getValue(props, "com.tugalsan.gvm.http.Main_sslRedirect", "true");
+        d.ci("construtor", "redirectToSSLStr", redirectToSSLStr);
+        redirectToSSL = TGS_UnSafe.call((() -> Boolean.valueOf(redirectToSSLStr)), e -> TGS_UnSafe.thrwReturns(new RuntimeException("ERROR for sslRedirectStr: Cannot convert String to Boolean: " + redirectToSSLStr)));
+        var sslPortStr = TS_FilePropertiesUtils.getValue(props, "com.tugalsan.gvm.http.Main_sslPort", "8081");
+        d.ci("construtor", "sslPortStr", sslPortStr);
+        sslPort = TGS_UnSafe.call((() -> Integer.valueOf(sslPortStr)), e -> TGS_UnSafe.thrwReturns(new RuntimeException("ERROR for sslPortStr: Cannot convert String to Integer: " + sslPortStr)));
+        var sslPathStr = TS_FilePropertiesUtils.getValue(props, "com.tugalsan.gvm.http.Main_sslPath", "D:/xampp_data/SSL/tomcat.p12");
+        d.ci("construtor", "sslPathStr", sslPathStr);
+        sslPath = TGS_UnSafe.call((() -> Path.of(sslPathStr)), e -> TGS_UnSafe.thrwReturns(new RuntimeException("ERROR for sslPathStr: Cannot convert String to Path: " + sslPathStr)));
+        sslPass = TS_FilePropertiesUtils.getValue(props, "com.tugalsan.gvm.http.Main_sslPass", "MyPass");
+        d.ci("construtor", "sslPass", sslPass);
+        var pathFileServerStr = TS_FilePropertiesUtils.getValue(props, "com.tugalsan.gvm.http.Main_pathFileServer", "D:/file");
+        d.ci("construtor", "pathFileServerStr", pathFileServerStr);
+        pathFileServer = TGS_UnSafe.call((() -> Path.of(pathFileServerStr)), e -> TGS_UnSafe.thrwReturns(new RuntimeException("ERROR for pathFileServerStr: Cannot convert String to Path: " + pathFileServerStr)));
+        if (!propsExists) {
+            TS_FilePropertiesUtils.write(props, propsFile);
+        }
+    }
+    final public boolean redirectToSSL;
+    final public int sslPort;
+    final public Path sslPath, pathFileServer;
+    final public String ip, sslPass;
+
+    public static Settings of(Path propsFile) {
+        return new Settings(propsFile);
+    }
+
+}
