0da57e040f17f0406d5a859161078bc7f0f4269f
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-12 13:05:40 +0300

    Update Main.java

--- src/main/java/com/tugalsan/tst/servlet/http/Main.java
+++ src/main/java/com/tugalsan/tst/servlet/http/Main.java
@@ -4,6 +4,7 @@ import com.tugalsan.api.file.client.TGS_FileTypes;
 import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.servlet.http.server.TS_SHttpConfigNetwork;
 import com.tugalsan.api.servlet.http.server.TS_SHttpConfigSSL;
+import com.tugalsan.api.servlet.http.server.TS_SHttpHandlerRequest;
 import com.tugalsan.api.servlet.http.server.TS_SHttpHandlerText;
 import com.tugalsan.api.servlet.http.server.TS_SHttpServer;
 import com.tugalsan.api.tuple.client.TGS_Tuple2;
@@ -20,13 +21,19 @@ public class Main {
     //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.serialcom-1.0-SNAPSHOT-jar-with-dependencies.jar    
     public static void main(String[] args) {
         var settings = Settings.of(Settings.pathDefault());
-        TGS_ValidatorType1<TGS_UrlParser> allow = parser -> true;
-        var customTextHandler = TS_SHttpHandlerText.of("/", allow, request -> {
+        TGS_ValidatorType1<TS_SHttpHandlerRequest> allow = request -> {
             if (!request.isLocal()) {
                 request.sendError404("ERROR: i am grumpy, and will work only localhost 😠");
-                return null;
+                return false;
             }
-            return TGS_Tuple2.of(TGS_FileTypes.htm_utf8, "<html><head><script>location.reload();</script></head><body>" + request.url + "<body></html>");
+            return true;
+        };
+        var customTextHandler = TS_SHttpHandlerText.of("/", allow, request -> {
+            return TGS_Tuple2.of(TGS_FileTypes.htm_utf8,
+                    "<html><head><script>location.reload();</script></head><body>"
+                    + request.url
+                    + "<body></html>"
+            );
         });
         var network = TS_SHttpConfigNetwork.of(settings.ip, settings.sslPort);
         var ssl = TS_SHttpConfigSSL.of(settings.sslPath, settings.sslPass, settings.redirectToSSL);
